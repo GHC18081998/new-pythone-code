a8487e682972bf59e3cfd1d20f8d123cac176765
Chandu <106138199+GHC18081998@users.noreply.github.com>
2026-04-29 11:00:01 +0530

    Modify start.sh to use private Docker registry
    Updated the script to pull Docker image from a private registry and login using AWS SSM parameters.

---
+++ scripts/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+REGISTRY_URL=$(aws ssm get-parameter --name "/myapp/docker/registry_url" --query "Parameter.Value" --output text)
+
+# Login again on the EC2 instance to pull the image
+DOCKER_USER=$(aws ssm get-parameter --name "/myapp/docker/username" --query "Parameter.Value" --output text)
+DOCKER_PASS=$(aws ssm get-parameter --name "/myapp/docker/password" --with-decryption --query "Parameter.Value" --output text)
+echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin $REGISTRY_URL
+
+docker pull $REGISTRY_URL:latest
+docker run -d -p 80:5000 --name flask-app $REGISTRY_URL:latest
