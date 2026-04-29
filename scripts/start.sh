8cf9add5b196088f87fc7ae3fb546049fc7ff913
Chandu <106138199+GHC18081998@users.noreply.github.com>
2026-04-29 19:02:37 +0530

    Change port mapping for flask-app container

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,4 +7,4 @@ DOCKER_PASS=$(aws ssm get-parameter --name "/myapp/docker/password" --with-decry
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin $REGISTRY_URL
 
 docker pull $REGISTRY_URL:latest
-docker run -d -p 80:5000 --name flask-app $REGISTRY_URL:latest
+docker run -d -p 5000:5000 --name flask-app $REGISTRY_URL:latest
